eae9a6a5d761ed073d37afb9f84d3efb1199fa1d
Stephen Belanger <admin@stephenbelanger.com>
2025-03-27 18:35:11 +0800

    Remove unused scripts and fix Embed startup

--- scripts/download-php.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-set -euo pipefail
-
-PHP_VERSION=${1:-8.4}
-EXTENSIONS=${2:-$(source ./scripts/extensions.sh)}
-
-./spc download --prefer-pre-built --with-php=${PHP_VERSION} --retry=10 --for-extensions=${EXTENSIONS}
