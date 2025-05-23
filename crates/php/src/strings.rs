e8b3548c2ec24aad0b36c23569e997cdd54675ff
Stephen Belanger <admin@stephenbelanger.com>
2025-05-23 22:35:37 +0800

    Improve exceptions

--- crates/php/src/strings.rs
+++ crates/php/src/strings.rs
@@ -28,14 +28,14 @@ pub(crate) fn nullable_cstr<S: Into<String>>(
 
 pub(crate) fn cstr<S: AsRef<str>>(s: S) -> Result<*mut c_char, EmbedException> {
   CString::new(s.as_ref())
-    .map_err(EmbedException::InvalidCString)
+    .map_err(|_| EmbedException::CStringEncodeFailed(s.as_ref().to_owned()))
     .map(|cstr| cstr.into_raw())
 }
 
 pub(crate) fn str_from_cstr<'a>(ptr: *const c_char) -> Result<&'a str, EmbedException> {
   unsafe { CStr::from_ptr(ptr) }
     .to_str()
-    .map_err(EmbedException::InvalidStr)
+    .map_err(|_| EmbedException::CStringDecodeFailed(ptr.addr()))
 }
 
 #[allow(dead_code)]
@@ -53,9 +53,8 @@ pub(crate) fn drop_str(ptr: *const i8) {
 
 pub(crate) fn maybe_current_dir() -> Result<PathBuf, EmbedException> {
   current_dir()
-    .unwrap_or(PathBuf::from("/"))
-    .canonicalize()
-    .map_err(EmbedException::IoError)
+    .and_then(|dir| dir.canonicalize())
+    .or(Err(EmbedException::FailedToFindCurrentDirectory))
 }
 
 pub(crate) fn translate_path<D, P>(docroot: D, request_uri: P) -> Result<PathBuf, EmbedException>
@@ -67,13 +66,22 @@ where
   let request_uri = request_uri.as_ref();
   let relative_uri = request_uri
     .strip_prefix("/")
-    .map_err(EmbedException::RelativizeError)?;
+    .map_err(|_| {
+      let uri = request_uri.display().to_string();
+      EmbedException::ExpectedAbsoluteRequestUri(uri)
+    })?;
 
-  match docroot.join(relative_uri).join("index.php").canonicalize() {
-    Ok(path) => Ok(path),
-    Err(_) => docroot
-      .join(relative_uri)
-      .canonicalize()
-      .map_err(EmbedException::CanonicalizeError),
-  }
+  let exact = docroot
+    .join(relative_uri);
+
+  exact
+    .join("index.php")
+    .canonicalize()
+    .or_else(|_| {
+      exact
+        .canonicalize()
+        .map_err(|_| {
+          EmbedException::ScriptNotFound(exact.display().to_string())
+        })
+    })
 }
